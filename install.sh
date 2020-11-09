d7977028f9cc33f5ca33ce06132a23647395dc5a
AlteredCoder <AlteredCoder>
2020-11-09 18:10:30 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -15,20 +15,10 @@ requirement() {
     mkdir -p "${LIB_PATH}"
 }
 
-check_apikeygen() {
-    echo "if you are on a single-machine setup, do you want the wizard to configure your API key ? (Y/n)"
-    echo "(note: if you didn't understand the question, 'Y' might be a safe answer)"
-    read answer
-    if [[ ${answer} == "" ]]; then
-            answer="y"
-    fi
-    if [ "$answer" != "${answer#[Yy]}" ] ;then
-            SUFFIX=`tr -dc A-Za-z0-9 </dev/urandom | head -c 8`
-            API_KEY=`cscli bouncers add cs-nginx-bouncer-${SUFFIX} -o raw`
-            API_KEY=${API_KEY} envsubst < ./config/crowdsec.conf > "${CONFIG_PATH}crowdsec.conf"
-    else 
-        echo "For your bouncer to be functionnal, you need to create an API key and set it in the ${CONFIG_PATH}crowdsec.conf file"
-    fi;
+gen_apikey() {
+    SUFFIX=`tr -dc A-Za-z0-9 </dev/urandom | head -c 8`
+    API_KEY=`cscli bouncers add cs-nginx-bouncer-${SUFFIX} -o raw`
+    API_KEY=${API_KEY} envsubst < ./config/crowdsec.conf > "${CONFIG_PATH}crowdsec.conf"
 }
 
 check_nginx_dependency() {
@@ -63,5 +53,5 @@ install() {
 
 requirement
 check_nginx_dependency
-check_apikeygen
+gen_apikey
 install
