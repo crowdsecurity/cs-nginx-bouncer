238cf98ed1701daecd11d88f6309ecb46d2cf275
NorthDakota <pol.vova@gmail.com>
2021-03-26 01:33:46 +0200

    Add support for http_x_forwarded_for

--- nginx/access.lua
+++ nginx/access.lua
@@ -1,12 +1,17 @@
 -- Convert the Ip to integer, and check if present in sqlite
-ok, err = require "CrowdSec".allowIp(ngx.var.remote_addr)
-if err ~= nil then 
+remoteAddr = ngx.var.remote_addr;
+if ngx.var.http_x_forwarded_for then
+    remoteAddr = string.match(ngx.var.http_x_forwarded_for, "^([^,]*)")
+end
+
+ok, err = require "CrowdSec".allowIp(remoteAddr)
+if err ~= nil then
     ngx.log(ngx.ERR, "[Crowdsec] bouncer error " .. err)
 end
 if ok == nil then
    ngx.log(ngx.ERR, "[Crowdsec] " .. err)
 end
 if not ok then
-    ngx.log(ngx.ERR, "[Crowdsec] denied '" .. ngx.var.remote_addr .. "'")
+    ngx.log(ngx.ERR, "[Crowdsec] denied '" .. remoteAddr .. "'")
 	ngx.exit(ngx.HTTP_FORBIDDEN)
 end
