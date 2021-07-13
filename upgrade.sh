c10c5d1cc44336d0ef5c275597c7fa5952ce4f60
blotus <sebastien@crowdsec.net>
2021-07-13 13:38:37 +0200

    migrate the old configuration if it exists on update && make upgrade/uninstall script executable (#10)

--- upgrade.sh
+++ upgrade.sh
@@ -6,6 +6,8 @@ NGINX_CONF_DIR="/etc/nginx/conf.d/"
 ACCESS_FILE="access.lua"
 LIB_PATH="/usr/local/lua/crowdsec/"
 CONFIG_PATH="/etc/crowdsec/bouncers/"
+CONFIG_FILE="${CONFIG_PATH}crowdsec-nginx-bouncer.conf"
+OLD_CONFIG_FILE="/etc/crowdsec/crowdsec-nginx-bouncer.conf"
 
 requirement() {
     cd $LUA_MOD_DIR
@@ -17,6 +19,17 @@ requirement() {
 install() {
 	cp nginx/${NGINX_CONF} ${NGINX_CONF_DIR}/${NGINX_CONF}
 	cp nginx/${ACCESS_FILE} ${LIB_PATH}
+}
+
+migrate_conf() {
+    if [ -f "$CONFIG_FILE" ]; then
+        return
+    fi
+    if [ ! -f "$OLD_CONFIG_FILE" ]; then
+        return
+    fi
+    echo "Found $OLD_CONFIG_FILE, but no $CONFIG_FILE. Migrating it."
+    mv "$OLD_CONFIG_FILE" "$CONFIG_FILE"
 }
 
 if ! [ $(id -u) = 0 ]; then
@@ -31,4 +44,5 @@ fi
 
 requirement
 install
+migrate_conf
 echo "crowdsec-nginx-bouncer upgraded successfully"
