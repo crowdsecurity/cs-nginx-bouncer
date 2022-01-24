89e04d85e9ca3757e3f4d230ab2dd2ba6c54d975
alteredCoder <kevin@crowdsec.net>
2022-01-24 12:29:47 +0100

    update new path

--- install.sh
+++ install.sh
@@ -49,12 +49,10 @@ install() {
     cp ${LUA_MOD_DIR}/nginx/access.lua ${LIB_PATH}
     cp ${LUA_MOD_DIR}/nginx/recaptcha.lua ${LIB_PATH}
     cp ${LUA_MOD_DIR}/nginx/iputils.lua ${LIB_PATH}/plugins/crowdsec/
-    cp ${LUA_MOD_DIR}/nginx/bitop.lua ${LIB_PATH}
+    cp ${LUA_MOD_DIR}/nginx/bitop.lua ${LIB_PATH}/plugins/crowdsec/
 
     luarocks install lua-resty-http
     luarocks install lua-cjson
-    luarocks install lua-resty-ipmatcher
-    luarocks install lua-resty-core
 }
 
 
